ce8ff473b0e2e57ed1463985672e03632614e08f
ramin keene <raminkeene@gmail.com>
2015-03-13 20:53:25 -0400

    require 'activesupport' too to prevent https://github.com/rails/rails/issues/12876 outside of rails environments

--- lib/multicolor.rb
+++ lib/multicolor.rb
@@ -1,3 +1,4 @@
+require 'active_support'
 require 'active_support/core_ext'
 require 'faraday'
 require 'faraday_middleware'
--- lib/multicolor/version.rb
+++ lib/multicolor/version.rb
@@ -1,3 +1,3 @@
 module Multicolor
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
